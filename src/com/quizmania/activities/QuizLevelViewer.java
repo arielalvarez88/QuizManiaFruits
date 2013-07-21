13ae940c7989b3bd512a41b2aba5f12387a05d08
Ariel <arielalvarez88@gmail.com>
2013-07-21 15:43:17 -0400

    Changing fruits when you click the image

--- src/com/quizmania/activities/QuizLevelViewer.java
+++ src/com/quizmania/activities/QuizLevelViewer.java
@@ -3,11 +3,11 @@ package com.quizmania.activities;
 import java.io.IOException;
 import java.util.List;
 
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.widget.ImageView;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
 
 import com.example.quizmaniafruits.R;
 import com.quizmaina.entities.QuizElement;
@@ -17,6 +17,7 @@ import com.quizmania.utils.QuizElementsLoader;
 public class QuizLevelViewer extends FragmentActivity {
 	List<QuizElement> elements;
 	FragmentManager fragmentManager;
+	int currentSlide=0;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -32,7 +33,30 @@ public class QuizLevelViewer extends FragmentActivity {
 				.add(R.id.fragment_container, quizLevelFragment).commit();
 
 	}
+	
+	
+	public void fruitViewClick(View view){
+		System.out.println("CLICK!");
+		currentSlide++;
+		if(currentSlide >= elements.size()){
+			return;
+		}
+		
+		QuizMainFragment test = new QuizMainFragment();
+		test.setElement(elements.get(currentSlide));
+		FragmentTransaction tx=  fragmentManager.beginTransaction();
+		
+		tx.replace(R.id.fragment_container, test);
+		
+		tx.addToBackStack(null);
 
+		
+		tx.commit();
+		
+	}
+
+	
+	
 	private List<QuizElement> initializeQuizElements() {
 		try {
 			elements = QuizElementsLoader
